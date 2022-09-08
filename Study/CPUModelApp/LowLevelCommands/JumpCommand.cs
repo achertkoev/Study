d7c51fd2926ce12a9e51052258442e79fb1846dd
Albert Chertkoev <Betak6630@gmail.com>
2022-09-08 10:58:18 +0300

    Добаил проект CPUModelApp

---
+++ Study/CPUModelApp/LowLevelCommands/JumpCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class JumpCommand : ICommand
+{
+    public void Dump()
+    {
+        Console.Write("jmp");
+    }
+
+    public void Execute(int[] registers, ref int currentCommandIdex)
+    {
+        currentCommandIdex += registers[0];
+    }
+}
